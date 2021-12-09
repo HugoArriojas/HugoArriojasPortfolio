610841372424672815e8fbece7e417fa0ba89cfa
HugoArriojas <hugo.arriojas@hotmail.com>
2021-12-09 17:28:15 -0500

    added sideNav functionality and styled openSideNav button

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ function myFunction() {
       }
     }
   } 
+
+
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("mainContent").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("mainContent").style.marginLeft = "0";
+  } 
