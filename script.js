4c591ef975c8d553ade1c886978b00af8a06f17a
HugoArriojas <hugo.arriojas@hotmail.com>
2021-12-08 19:25:20 -0500

    added styling to about section

---
+++ script.js
@@ -0,0 +1,17 @@
+// mobileMenuOpener: function(){
+//     var hamburger		= $('.rewall_fn_mobilemenu_wrap .hamburger');
+//     hamburger.on('click',function(){
+//         var element 	= $(this);
+//         var menupart	= $('.rewall_fn_mobilemenu_wrap .mobilemenu');
+//         if(element.hasClass('is-active')){
+//             element.removeClass('is-active');
+//             menupart.removeClass('opened');
+//             menupart.slideUp(500);
+//         }else{
+//             element.addClass('is-active');
+//             menupart.addClass('opened');
+//             menupart.slideDown(500);
+//         }return false;
+//     });	
+// },
+
