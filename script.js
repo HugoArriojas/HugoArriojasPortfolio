bd8c279e4b689a61d984eebbb555f5f0dd034e5d
HugoArriojas <hugo.arriojas@hotmail.com>
2021-12-10 01:55:13 -0500

    changed styling of sideNav to look better on larger screens and added a larger contact section

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+
+//  Dropdown menu styling
+  // From W3Schools
 function openDropdown() {
     document.getElementById("menuLinks").classList.toggle("show");
   }
-  
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.menuIcon')) {
       const dropdowns = document.getElementsByClassName("mobileLinks");
       for (let i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        let openMenu = dropdowns[i];
+        if (openMenu.classList.contains('show')) {
+          openMenu.classList.remove('show');
         }
       }
     }
   } 
 
-
-
+// SideNav Styling
   /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("sideNav").style.width = "250px";
-    document.getElementById("mainContent").style.marginLeft = "250px";
+    document.getElementById("sideNav").style.width = "275px";
+    document.getElementById("mainContent").style.marginLeft = "275px";
   }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+// Setting nav width to 0
   function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     document.getElementById("mainContent").style.marginLeft = "0";
   } 
+
+  const skillItems = Array.from(document.getElementsByClassName('skillItem'));
+
+// Easter Egg
+console.log("Hey thanks for being so thorough in your search! Hope you like what you see 😊")
